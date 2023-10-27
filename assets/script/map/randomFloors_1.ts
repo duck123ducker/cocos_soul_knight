fac5237afe7f6a26c237ead69d74908c7ace00f8
duck123ducker <tencentmail@duck.com>
2023-10-27 16:02:50 +0800

    optimize draw calls

--- assets/script/map/randomFloors_1.ts
+++ assets/script/map/randomFloors_1.ts
@@ -1,19 +1,50 @@
-import { _decorator, Component, Node, Prefab, instantiate } from 'cc';
+import { _decorator, Component, Node, Sprite, Layers, UITransform, resources, SpriteFrame } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('randomFloors_1')
 export class randomFloors_1 extends Component {
-    @property(Prefab) randomFloor: Prefab
     width: number
     height: number
+    floors: SpriteFrame[]
+    floorsResult = {}
     start() {
-        for(let i = 0; i < this.width; i++){
-            for(let j = 0; j < this.height; j++){
-                const node = instantiate(this.randomFloor);
-                node.setPosition(i*16,j*16)
-                node.setParent(this.node)
+        resources.loadDir("maps/floor", SpriteFrame,
+            (_err, spriteFrames) => {
+                this.floors = spriteFrames
+                for(let i = 0; i < this.width; i++){
+                    for(let j = 0; j < this.height; j++){
+                        const tmpIndex = Math.floor(Math.random() * spriteFrames.length)
+                        if(!!this.floorsResult[tmpIndex]){
+                            this.floorsResult[tmpIndex].push({
+                                x: i*16,
+                                y: j*16
+                            })
+                        }else {
+                            this.floorsResult[tmpIndex] = [{
+                                x: i*16,
+                                y: j*16
+                            }]
+                        }
+                    }
+                }
+                Object.keys(this.floorsResult).forEach(key=>{
+                    this.floorsResult[key].forEach(pos=>{
+                        this.initFloor(pos.x,pos.y,key)
+                    })
+                })
             }
-        }
+        )
+    }
+
+    initFloor(x,y,index){
+        const node = new Node("")
+        node.addComponent(Sprite)
+        node.layer = Layers.Enum.UI_2D
+        node.setScale(1, 1.01)
+        node.getComponent(UITransform).setAnchorPoint(0,0)
+        node.getComponent(Sprite).spriteFrame = this.floors[index];
+        node.setPosition(x,y)
+        node.setParent(this.node)
     }
 
     update(deltaTime: number) {
--- assets/script/map/randomWalls_1.ts
+++ assets/script/map/randomWalls_1.ts
@@ -1,38 +1,111 @@
-import { _decorator, Component, Node, Prefab, instantiate, Sprite, UITransform, resources, SpriteFrame, Layers } from 'cc';
+import { _decorator, Component, Node, Sprite, UITransform, resources, SpriteFrame, Layers } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('randomWalls_1')
 export class randomWalls_1 extends Component {
-    @property(Prefab) randomWall: Prefab
     width: number
     height: number
+    wallTop :SpriteFrame[] = []
+    wallTopResult = {}
+    wallSide :SpriteFrame[] = []
+    wallSideResult = {}
+    shadow: SpriteFrame
+    shadowResult = []
+
     start() {
-        resources.load('white-bg/spriteFrame',SpriteFrame,(_err, spriteFrame) => {
-            for(let i = 0; i < this.width + 2; i++){
-                for(let j = this.height + 1; j >= 0 ; j--){
-                    const node = instantiate(this.randomWall);
-                    if(!(i>0&&i<this.width+1&&j>0&&j<this.height+1)){
-                        node.setPosition(i*16,j*16)
-                        node.setParent(this.node)
+        resources.loadDir("maps/wall", SpriteFrame,(_err, spriteFrames) => {
+            resources.load('white-bg/spriteFrame',SpriteFrame,(_err, spriteFrame) => {
+                this.shadow = spriteFrame
+                spriteFrames.forEach(item=>{
+                    if(item.getRect().height===8) this.wallSide.push(item)
+                    else if(item.getRect().height===16) this.wallTop.push(item)
+                    for(let i = 0; i < this.width + 2; i++){
+                        for(let j = this.height + 1; j >= 0 ; j--){
+                            if(!(i>0&&i<this.width+1&&j>0&&j<this.height+1)){
+                                const tmpTopIndex = Math.floor(Math.random() * this.wallTop.length)
+                                if(!!this.wallTopResult[tmpTopIndex]){
+                                    this.wallTopResult[tmpTopIndex].push({
+                                        x: i*16,
+                                        y: j*16+8
+                                    })
+                                }else {
+                                    this.wallTopResult[tmpTopIndex] = [{
+                                        x: i*16,
+                                        y: j*16+8
+                                    }]
+                                }
+                                if((j===0)||(j===this.height + 1&&i!==0&&i!==this.width+1)){
+                                    const tmpSideIndex = Math.floor(Math.random() * this.wallSide.length)
+                                    if(!!this.wallSideResult[tmpSideIndex]){
+                                        this.wallSideResult[tmpSideIndex].push({
+                                            x: i*16,
+                                            y: j*16
+                                        })
+                                    }else {
+                                        this.wallSideResult[tmpSideIndex] = [{
+                                            x: i*16,
+                                            y: j*16
+                                        }]
+                                    }
+                                }
+                            }
+                        }
+                        if(i>0&&i<this.width+1){
+                            this.shadowResult.push({x: i*16, y: this.height*16+8})
+                        }
                     }
-                }
-                if(i>0&&i<this.width+1){
-                    const node = new Node("shadow")
-                    node.addComponent(Sprite)
-                    node.layer = Layers.Enum.UI_2D
-                    node.getComponent(UITransform).setAnchorPoint(0,0)
-                    node.setScale(16,8)
-                    node.getComponent(Sprite).spriteFrame = spriteFrame;
-                    node.getComponent(Sprite).color.set(0,0,0,130)
-                    node.setPosition(i*16, this.height*16+8)
-                    node.setParent(this.node)
-                }
-            }
+                    Object.keys(this.wallTopResult).forEach(key=>{
+                        this.wallTopResult[key].forEach(pos=>{
+                            this.addWallTop(pos.x,pos.y,key)
+                        })
+                    })
+                    Object.keys(this.wallSideResult).forEach(key=>{
+                        this.wallSideResult[key].forEach(pos=>{
+                            this.addWallSide(pos.x,pos.y,key)
+                        })
+                    })
+                    this.shadowResult.forEach(pos=>{
+                        this.addShadow(pos.x,pos.y)
+                    })
+                })
+            })
         })
     }
 
+    addWallTop(x,y,indexTop){
+        const node = new Node()
+        node.addComponent(Sprite)
+        node.layer = Layers.Enum.UI_2D
+        node.setScale(1, 1.01)
+        node.getComponent(UITransform).setAnchorPoint(0,0)
+        node.getComponent(Sprite).spriteFrame = this.wallTop[indexTop];
+        node.setPosition(x,y)
+        node.setParent(this.node)
+    }
+
+    addWallSide(x,y,indexSide){
+        const node = new Node()
+        node.addComponent(Sprite)
+        node.layer = Layers.Enum.UI_2D
+        node.setScale(1, 1.01)
+        node.getComponent(UITransform).setAnchorPoint(0,0)
+        node.getComponent(Sprite).spriteFrame = this.wallSide[indexSide];
+        node.setPosition(x,y)
+        node.setParent(this.node)
+    }
+
+    addShadow(x,y){
+        const node = new Node("shadow")
+        node.addComponent(Sprite)
+        node.layer = Layers.Enum.UI_2D
+        node.getComponent(UITransform).setAnchorPoint(0,0)
+        node.setScale(16,8)
+        node.getComponent(Sprite).spriteFrame = this.shadow;
+        node.getComponent(Sprite).color.set(0,0,0,50)
+        node.setPosition(x, y)
+        node.setParent(this.node)
+    }
     update(deltaTime: number) {
-        
     }
 }
 
